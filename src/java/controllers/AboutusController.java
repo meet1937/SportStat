01f30d1b31af7018f703aa230688624a13bc1188
Dhiraj5509 <76967758+Dhiraj5509@users.noreply.github.com>
2021-05-18 11:57:32 +0530

    About us controller

---
+++ src/java/controllers/AboutusController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import entities.Sports;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Controller
+public class AboutusController {
+     @RequestMapping(value = "/about")
+    public ModelAndView giveInitialPage() {
+        
+        ModelAndView model = new ModelAndView("Aboutus");
+       
+        return model;
+    }
+}
